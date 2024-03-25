83dcb971efea6e2391edaa9b350fe94be52615bc
Youssef <mr.boualiyoussef@gmail.com>
2024-03-25 04:07:00 +0000

    vite runned

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // or '0.0.0.0' to listen on all network interfaces
+    port: 9400, // specify your desired port
+  },
 })
