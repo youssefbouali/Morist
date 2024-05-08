bbe03e5d42e47dbf0b28b472a2784fd308305638
Bouali <youssefbouali206@gmail.com>
2024-05-08 17:15:32 +0100

    3

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,25 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+/*Route::post('/user', function (*//*Request $request*//*) {
+	//$userData = $request->all();
+    return ['user' => "h"];
+});
+
+Route::get('/user', function () {
+    return ['user' => "{0:{'username':'u1'},1:{'username':'u2'}}"];
+});
+
+Route::get('/user/{user}', function ($user1) {
+    return ['user' => $user1];
+});
+
+Route::put('/user', function () {
+    return ['user' => "Updated"];
+});
+
+Route::delete('/user', function () {
+    return ['user' => "Deleted"];
+});*/
+
 require __DIR__.'/auth.php';
